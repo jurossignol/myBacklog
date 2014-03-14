044b41e35b35f1211d322ebdf6d0e9c471f44099
unknown <j.rossignol@MW7F9KED0UQ80F.dir.svc.accenture.com>
2014-03-15 00:54:09 +0100

    some dummy test with json

--- web/mybacklog.dart
+++ web/mybacklog.dart
@@ -1,9 +1,23 @@
 import 'dart:html';
+import 'dart:convert' show JSON;
+import 'dart:async' show Future;
 
 void main() {
   querySelector("#sample_text_id")
       ..text = "Click me!"
       ..onClick.listen(reverseText);
+  
+  HttpRequest.getString('data.json').then(manageUsers)
+	 .catchError((err){
+	  	print("call ko $err");
+	  });
+  
+  
+}
+
+void manageUsers(String response){
+  	List users = JSON.decode(response);
+    print(users);
 }
 
 void reverseText(MouseEvent event) {
@@ -13,4 +27,10 @@ void reverseText(MouseEvent event) {
     buffer.write(text[i]);
   }
   querySelector("#sample_text_id").text = buffer.toString();
+}
+
+class User{
+	
+	String _firstName;
+	String _lastName;
 }
