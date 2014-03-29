bfb821ccd90073db223d2b92508c5427cc734662
unknown <j.rossignol@MW7F9KED0UQ80F.dir.svc.accenture.com>
2014-03-29 19:50:57 +0100

    Lecture fichier json pour affichage liste user

--- web/mybacklog.dart
+++ web/mybacklog.dart
@@ -4,33 +4,39 @@ import 'dart:async' show Future;
 
 void main() {
   querySelector("#sample_text_id")
-      ..text = "Click me!"
-      ..onClick.listen(reverseText);
-  
-  HttpRequest.getString('data.json').then(manageUsers)
-	 .catchError((err){
-	  	print("call ko $err");
-	  });
-  
-  
+      ..text = "GET USERS LIST!"
+      ..onClick.listen(getUsersList);
+}
+
+void getUsersList(MouseEvent event) {
+	HttpRequest.getString('data.json').then(manageUsers)
+  	 .catchError((err){
+  	  	print("call ko $err");
+  	  });
 }
 
 void manageUsers(String response){
-  	List users = JSON.decode(response);
-    print(users);
-}
-
-void reverseText(MouseEvent event) {
-  var text = querySelector("#sample_text_id").text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
-  }
-  querySelector("#sample_text_id").text = buffer.toString();
+	String text = "";
+	List<User> users = new List<User>();
+	List jsonUsers = JSON.decode(response);
+	jsonUsers.forEach((map) => users.add(new User.fromJson(map)));
+	querySelector("#sample_text_id")
+        ..text = users.toString();
 }
 
 class User{
-	
 	String _firstName;
 	String _lastName;
+	
+	User({String firstName, String lastName}) {
+      _firstName = firstName;
+      _lastName = lastName;
+	}
+	
+	User.fromJson(Map jsonMap) {
+      _firstName = jsonMap['firstName'];
+      _lastName = jsonMap['lastName'];
+  }
+	
+	String toString() => "${_firstName} ${_lastName}";
 }
